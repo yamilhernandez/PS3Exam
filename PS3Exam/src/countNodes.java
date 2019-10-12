7b462a4a65f93c1dbfccc2fc5cce5b710d35199e
ibrah <ibrah@ibrah-Satellite-C55t-C>
2019-10-12 18:01:10 -0400

    CountNodes done

---
+++ PS3Exam/src/countNodes.java
@@ -0,0 +1,11 @@
+
+public class countNodes {
+	
+	
+	  public int countNodes(TreeNode root) {
+	        if (root==null) return 0;
+	        return 1+ countNodes(root.left)+ countNodes(root.right);
+	        
+	    }
+
+}
