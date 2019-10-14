a52322339e745524225ca7e474b7753063c41f40
ibrah <ibrah@ibrah-Satellite-C55t-C>
2019-10-14 11:38:11 -0400

    Merge k sorted list done

--- PS3Exam/src/deleteNode.java
+++ PS3Exam/src/deleteNode.java
@@ -4,16 +4,8 @@ public class deleteNode {
 public TreeNode deleteNode(TreeNode root, int key) {
 	TreeNode node= root;
 	
-	while (node.val!=key) {
-		if(node.right==null && node.left==null) return root;
-		if(node.val<key) {
-			
-			node=node.right;
-		}
-		if (node.val>key) {
-			node=node.left;
-		}
-	}
+	
+	
 	
 	
 	
--- PS3Exam/src/mergeKList.java
+++ PS3Exam/src/mergeKList.java
@@ -1,9 +1,27 @@
+import java.util.PriorityQueue;
 
 public class mergeKList {
 	 public ListNode mergeKLists(ListNode[] lists) {
 		 
+		 PriorityQueue<Integer> minHeap= new PriorityQueue<Integer>();
+		
+		 for (ListNode node : lists) {
+			 while(node!=null) {
+				 minHeap.add(node.val);
+				 node=node.next;
+			 }
+			
+		}
+		 ListNode dummy= new ListNode(-1);
+		 ListNode head=dummy;
+		 while(!minHeap.isEmpty()) {
+			 head.next= new ListNode(minHeap.remove());
+			 head=head.next;
+			 
+		 }
 		 
-		return null;
+		 
+		return dummy.next;
 	        
 	    }
 
